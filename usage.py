1a3bbf154bae85ec1d07f555514d93fc8d8aa351
William ISABELLE <74108878+Aiveriwhair@users.noreply.github.com>
2022-02-13 12:33:22 +0100

    Create usage.py

---
+++ usage.py
@@ -0,0 +1,5 @@
+from mailSender import mailSender
+
+sender = mailSender("Mail sent with python", 
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor luctus quam.")
+sender.SendEmail()
