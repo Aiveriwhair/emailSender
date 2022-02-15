2e8d01f146d01b00c219d54c67c1f3f66390e05b
William ISABELLE <74108878+Aiveriwhair@users.noreply.github.com>
2022-02-15 14:11:54 +0100

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,5 +1,7 @@
 from mailSender import mailSender
 
 sender = mailSender("Mail sent with python", 
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor luctus quam.")
-sender.SendEmail()
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor luctus quam.",
+                    "sender@gmail.com",
+                    "gmailSenderPassword")
+sender.sendEmailTo(["receiver@gmail.com"])
