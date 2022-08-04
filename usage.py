e02943ab4e47784d107f2f6cf1e7bf469feb5982
William ISABELLE <74108878+Aiveriwhair@users.noreply.github.com>
2022-08-04 12:28:50 +0200

    Update usage.py

--- usage.py
+++ usage.py
@@ -4,4 +4,4 @@ sender = mailSender(subject="Mail sent with python",
                     body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor luctus quam.",
                     senderMail="sender@gmail.com",
                     senderPassw="gmailSenderPassword")
-sender.sendEmailTo(["receiver@gmail.com"])
+sender.sendEmailTo(["receiver1@gmail.com", receiver2@gmail.com])
